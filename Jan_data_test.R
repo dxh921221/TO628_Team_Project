1256115b7c5fe0d8a76cb9559eff8695bec04b13
Priti Singh <pritis@umich.edu>
2020-04-13 09:52:06 -0400

    study

--- Jan_data_test.R
+++ Jan_data_test.R
@@ -15,8 +15,13 @@ head(jandata_rand$tripduration)
 jandata_train <- jandata[1:9000, ]
 jandata_test  <- jandata[9001:19676, ]
 
+#Decision tree
 library(C50)
 jandata_model <- C5.0(jandata_train[], jandata_train$tripduration)
+
+#Linear model
+durationmodel <- lm(tripduration ~ starttime, data = jandata_train)
+summary(durationmodel)
 
 #Generalized linear model for prediction
 #logit.model <- glm(tripduration ~ starttime , data = jandata_train, family = "binomial")
