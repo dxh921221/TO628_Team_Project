dcb76ed2c4ce399e0930d0735457dd1e40a23021
Priti Singh <pritis@umich.edu>
2020-04-13 09:01:56 -0400

    change

--- Jan_data_test.R
+++ Jan_data_test.R
@@ -3,7 +3,7 @@ str(jandata)
 summary(jandata)
 #cleaning data
 jandata$bikeid <- NULL
-
+jandata$tripduration <- as.factor(jandata$tripduration)
 set.seed(12345)
 jandata_rand <- jandata[order(runif(1000)), ]
 
@@ -15,9 +15,12 @@ head(jandata_rand$tripduration)
 jandata_train <- jandata[1:9000, ]
 jandata_test  <- jandata[9001:19676, ]
 
+library(C50)
+jandata_model <- C5.0(jandata_train[], jandata_train$tripduration)
+
 #Generalized linear model for prediction
-logit.model <- glm(tripduration ~ starttime , data = jandata_train, family = "binomial")
-summary(logit.model)
+#logit.model <- glm(tripduration ~ starttime , data = jandata_train, family = "binomial")
+#summary(logit.model)
 
 #Predict outcomes
 
